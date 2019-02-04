d1c2992204dbf80cde0c9c8c4ea2ddb6ce4c70ae
Wannderson Oliveira <wannders.35@hotmail.com>
2019-02-04 18:04:39 -0200

    Update server.js
    CDU04 - Deletar compromisso - Feito - issue #4 
    CDU05 - Editar compromisso - Feito - issue #5

--- server.js
+++ server.js
@@ -44,6 +44,30 @@ app.get('/listar', function(req, res){
 	});
 });
 
+app.get('/deletar/:nmbr', function(req, res){
+	sqlite.run("delete from Compromisso where id = "+req.params.nmbr);
+	res.redirect("/listar");
+});
+
+app.get('/editar/:nmbr', function(req, res){
+	var compromisso = sqlite.run('select * from Compromisso where id = '+req.params.nmbr);
+	res.render('pages/editar', {
+		compr : compromisso
+	});
+});
+
+app.post('/edicao', function(req, res){
+	var comp = {desc: req.body.desc, local:req.body.local,horario: req.body.horario, data: req.body.data};
+
+	sqlite.run('delete from Compromisso where id = '+req.body.nmbr);
+
+	i = sqlite.insert('Compromisso', {descricao:comp.desc, local:comp.local, data:comp.data, horario: comp.horario}, function(res){
+		if(res.error){
+			throw res.error;
+		}});
+
+	res.redirect('/listar');
+});
 
 app.listen(8080);
 console.log('O servidor está rodando na porta 8080');
